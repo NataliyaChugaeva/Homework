b7f3765e96649661564e796d95c191961d90a80a
Nataliya <nat.georgieva2017@yandex.ru>
2022-06-30 02:11:26 +0300

    Не решила восьмую задачу

--- Task_08/Program.cs
+++ Task_08/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+ int index = 0;
+while (N >= a)
+{     
+   if (a%2==0)
+   {
+     index=index+1;
+     a = a + 2;
+   };
+   
+}
+Console.WriteLine($"");
