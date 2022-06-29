b250477d664ff8cfcf04ae65512e35e8be10d006
Nataliya <nat.georgieva2017@yandex.ru>
2022-06-29 22:52:00 +0300

    Решение второй задачи

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+ if (a>b)
+ {
+   max = a ;
+ }
+ else
+ {
+    max = b ;
+ }
+ Console.WriteLine($"max = {max}");
