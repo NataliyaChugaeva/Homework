89a75e8a9c93dd4fd42107e091d1fe1b9478cde8
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-03 21:31:25 +0300

    Решила задачу 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Ведите трехзначное целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int newNum1 = num/10;
+int newNum2 = newNum1%10;
+Console.WriteLine(newNum2);
