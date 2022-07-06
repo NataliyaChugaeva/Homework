7c23caab3016440239b040581d8263c9a058528e
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-06 23:06:56 +0300

    Закоммитила файлы

---
+++ Task_23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+//Console.Clear();
+Console.WriteLine("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n ; i++)
+{
+    Console.WriteLine($"|Куб числа {i,  10} = |{i * i * i,10}|");
+}
+
+//int i = 1;
+// while (i<=num)
+// {
+//     Console.WriteLine($"{i}     |     {i*i*i}");
+//     i++;
+// }
+
+
