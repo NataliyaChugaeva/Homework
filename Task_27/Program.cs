69aa1b8885cc483e145d363e93396c07ea91276b
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-12 21:34:51 +0300

    Решила двадцать седьмую задачу

---
+++ Task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int res = 0;
+ while (num > 0)
+ {
+    res = res + num%10;
+    num=num/10;
+ }
+Console.WriteLine(res);
