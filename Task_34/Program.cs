030b41614606fa5ec8f2bdfe365112a04cb6892d
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-17 23:52:04 +0300

    Решила задачи 34 и 36

---
+++ Task_34/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+Console.Write("Введите размер массива:");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int FindEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+            Console.Write("[");
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ",");
+        else
+            Console.Write(arr[i] + "]");
+    }
+    Console.WriteLine();
+}
+
+int[] array = CreateArrayRndInt(sizeArr,100,999);
+//PrintArray(array);
+int countEven = FindEven(array);
+Console.WriteLine("Количество четных чисел в массиве = " + countEven);
