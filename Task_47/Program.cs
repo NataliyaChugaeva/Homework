f0ff2fba1878b827202d7c0a4256520d2997a5eb
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-21 21:44:09 +0300

    Добавила файлы и решила 47 задачу

---
+++ Task_47/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int m, int n, int min , int max)
+{
+    double[,] arr = new double[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round(rnd.NextDouble() * (max - min), 1);
+        }
+    }   
+    return arr;
+}
+
+
+void PrintMatrix(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+          if (j == 0) Console.Write("|");
+          if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i,j], 6} | ");
+          else Console.Write($"{arr[i,j], 6} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] arrayResult = CreateMatrixRndDouble(3, 4, 1, 10);
+PrintMatrix(arrayResult);
---
+++ Task_50/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+
+int[,] CreateMatrixRndInt(int m, int n, int min , int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+          if (j == 0) Console.Write("|");
+          if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i,j], 3} | ");
+          else Console.Write($"{arr[i,j], 3} | ");
+        }
+        Console.WriteLine();
+    }
+}
