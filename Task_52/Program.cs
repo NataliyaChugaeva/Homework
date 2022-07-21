5dfa234336531323b4dcaa78e1e48cb79e322097
Nataliya <nat.georgieva2017@yandex.ru>
2022-07-21 22:47:34 +0300

    Добавила изменения в задачу 52

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -50,25 +50,3 @@ int[,] arrayResult = CreateMatrixRndInt(3, 4, 1, 10);
 PrintMatrix(arrayResult);
 
 
-// int[,] arr = new int[rnd.Next(1, 10), rnd.Next(1, 10)];
-// for (int i = 0; i < arr.GetLength(0); i++)
-// {
-//     for (int j = 0; j < arr.GetLength(1); j++)
-//     {
-//         arr[i, j] = rnd.Next(1, 10);
-//         Console.Write(arr[i, j] + " ");
-//     }
-//     Console.WriteLine();
-// }
-// Console.WriteLine("---------------------------");
-// Console.WriteLine(arr.GetLength(0));
-// for (int j = 0; j < arr.GetLength(1); j++)
-// {
-//     double sum = 0;
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         sum += arr[i, j];
-//     }
-//     Console.Write($"{ sum / arr.GetLength(0)} ");
-// }
-// Console.ReadLine();}
